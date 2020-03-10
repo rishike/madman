9485f6ba7df652d20c6fc5f65f619d90824f8f82
rishikesh singh <rishikesh@sculpturedminds.net>
2020-03-10 20:41:54 +0530

    pushing map work

--- madman/urls.py
+++ madman/urls.py
@@ -17,15 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import HomeView
-# from rest_framework import routers
-# from .api.serializer import UserViewSet
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-
-
-# router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
